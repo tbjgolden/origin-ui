f6f9adcb492226dc27434e827ae9dd2ed0d5c203
Tom <8162045+tbjgolden@users.noreply.github.com>
2022-05-21 17:17:01 +0100

    feat: initial commit after rebuild

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   "*.{tsx,ts}": [
-    "eslint --cache --fix --max-warnings=0 --rule 'no-console: [error,{allow:[warn,error]}]'",
+    "eslint --cache --fix --max-warnings=269 --rule 'no-console: [error,{allow:[warn,error]}]'",
     "prettier --write",
   ],
   "*.{pcss,css,js,json,html}": ["prettier --write"],
